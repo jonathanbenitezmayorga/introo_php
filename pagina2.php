a10cf6166af5134cf7d8f929220831ad6cb8d8eb
“jonathanbenitezmayorga” <jonathanbenitez@colegioguanenta.edu.co>
2024-07-29 18:14:23 -0500

    ejercicios

---
+++ pagina2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Sistemas</h1>
+<h2>Guanentá</h2>
+<hr>
+<?php
+$a = 10;
+$b = 15;
+echo ("La suma de 10 y 15 es: ". ($a+$b)."<br/>");
+echo ("El residuo de 15 y 10: ". ($b%$a)."<br/>");
+echo ("La variable \sa con incremento: ".(++$a)."<br/>");
+?>
+</body>
+</html>
